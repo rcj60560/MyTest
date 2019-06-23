190226b3521683cf4d34782abc7e0660e5810df9
rcj60560 <289759379@qq.com>
2019-06-23 22:04:04 +0800

    init 1

--- app/src/main/java/com/luocj/mytest/MainActivity.java
+++ app/src/main/java/com/luocj/mytest/MainActivity.java
@@ -2,6 +2,10 @@ package com.luocj.mytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.blankj.utilcode.util.ActivityUtils;
+import com.luocj.mytest.activity.RecyclerviewActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void doRecyclerview(View view) {
+        ActivityUtils.startActivity(RecyclerviewActivity.class);
     }
 }
--- app/src/main/java/com/luocj/mytest/activity/RecyclerviewActivity.java
+++ app/src/main/java/com/luocj/mytest/activity/RecyclerviewActivity.java
@@ -1,4 +1,15 @@
 package com.luocj.mytest.activity;
 
-public class RecyclerviewActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.luocj.mytest.R;
+
+public class RecyclerviewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recyclerview_test);
+    }
 }
