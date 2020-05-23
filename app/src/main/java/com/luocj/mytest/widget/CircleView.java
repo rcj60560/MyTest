307f6419fa05ef12e8a7c3b59bd9e64a1e2aa64b
luocj <289759379@qq.com>
2020-05-23 21:38:40 +0800

    views1

--- app/src/main/java/com/luocj/mytest/widget/CircleView.java
+++ app/src/main/java/com/luocj/mytest/widget/CircleView.java
@@ -5,6 +5,8 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -16,16 +18,9 @@ public class CircleView extends View {
     private int width;
     private Paint paint;
 
-    public CircleView(Context context) {
-        super(context);
-    }
 
     public CircleView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-    }
-
-    public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CircleView);
         width = (int) typedArray.getDimension(R.styleable.CircleView_width, 200);
         typedArray.recycle();
@@ -56,7 +51,16 @@ public class CircleView extends View {
         canvas.drawColor(Color.RED);
 
         paint.setColor(Color.YELLOW);
-        canvas.drawCircle(getWidth()/2.f,getHeight()/2.f,50,paint);
+//        canvas.drawCircle(getWidth() / 2.f, getHeight() / 2.f, 200, paint);
 
+        Rect rectF = new Rect();
+        rectF.left=0 + 15;
+        rectF.top =  0  + 15;
+        rectF.right = getWidth()/2;
+        rectF.bottom = getHeight()/2;
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(10);
+        canvas.drawRect(rectF,paint);
     }
 }
