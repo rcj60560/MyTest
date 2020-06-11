7d5d4d2589c1529c4b3ae07eaee4aaa505c55816
rcj60560 <63560560>
2020-06-11 16:08:09 +0800

    dev1

--- app/src/main/java/com/luocj/mytest/MainActivity.java
+++ app/src/main/java/com/luocj/mytest/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
     //test
     //mac commit
+    //我是master 分支
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
